6afbad44d034ea746de77dff8caea82fad8ce117
Andreas Touliatos <atouliatos43@gmail.com>
2026-03-13 09:09:33 +0000

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -19,7 +19,18 @@ const UI = {
 
         // Small delay so modal is visible before camera init
         setTimeout(() => {
-            this.scanner = new Html5Qrcode("reader");
+            this.scanner = new Html5Qrcode("reader", {
+                formatsToSupport: [
+                    Html5QrcodeSupportedFormats.QR_CODE,
+                    Html5QrcodeSupportedFormats.CODE_128,
+                    Html5QrcodeSupportedFormats.CODE_39,
+                    Html5QrcodeSupportedFormats.EAN_13,
+                    Html5QrcodeSupportedFormats.EAN_8,
+                    Html5QrcodeSupportedFormats.UPC_A,
+                    Html5QrcodeSupportedFormats.UPC_E,
+                    Html5QrcodeSupportedFormats.DATA_MATRIX
+                ]
+            });
             const config = { 
                 fps: 10, 
                 qrbox: { width: 250, height: 150 },
